1850fa9c4d7948e80ca6a870fa215164341ece9f
Julien GODEST <julien.godest@etu.univ-lyon1.fr>
2023-05-03 19:25:32 +0200

    Deploy

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({ out: 'public'})
+		adapter: adapter()
 	}, 
 	preprocess: vitePreprocess()
 };
