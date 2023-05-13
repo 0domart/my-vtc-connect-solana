7b22b4c126f1797eaa779bb0916dda92bec809d3
Julien GODEST <julien.godest@etu.univ-lyon1.fr>
2023-05-13 15:57:05 +0200

    bottom padding

--- src/routes/payment/+page.svelte
+++ src/routes/payment/+page.svelte
@@ -340,7 +340,7 @@ async function checkTransactionDone() {
                         </svg>
                         <span class="pl-2">{txnConfirmed? "Retour" : "Annuler"}</span></button>
                     </div>
-                    <div class="grid grid-flow-row justify-center items-center">
+                    <div class="grid grid-flow-row justify-center items-center pb-20">
                         <button on:click={refresh} class="btn normal-case w-80 btn-lg bg-[var(--primary-color)] text-[var(--secondary-color)]"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="inline w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v4m0 0v4m0-4h4m-4 0H8" />
                             <path d="M20.25 12C19.561 6.927 15.073 3 10 3S.438 6.927.75 12H3m2 0h4m4 0h4M3 12h1m1 0h4m4 0h1M9 16l2 2 2-2M12 2v4" />
