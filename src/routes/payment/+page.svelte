babdf84c5f2e331bd9d3b37191db39bfba7f9a84
Julien GODEST <julien.godest@etu.univ-lyon1.fr>
2023-05-13 15:49:33 +0200

    yellow en attente de validation

--- src/routes/payment/+page.svelte
+++ src/routes/payment/+page.svelte
@@ -318,7 +318,7 @@ async function checkTransactionDone() {
                     <div class="indicator justify-items-center place-self-center">
                         <div class="">
                             {#if !txnConfirmed}
-                            <span class="text-purple-400">
+                            <span class="text-[var(--primary-color)]">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                                 </svg>
