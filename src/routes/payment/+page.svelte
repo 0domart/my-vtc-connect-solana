4fa8d2c5695166ae28d8f06782b9453552c864b6
Julien GODEST <julien.godest@etu.univ-lyon1.fr>
2023-05-14 14:46:39 +0200

    tracking

--- src/routes/payment/+page.svelte
+++ src/routes/payment/+page.svelte
@@ -186,6 +186,7 @@ onDestroy(async () => {
     // <svg width=512 height=512 viewBox="-1 -1 2 2" bind:this={qrCode}/>
 })
 async function cancel() {
+    _paq.push(['trackEvent', 'Button', 'Click', 'ButtonLabel']);
     clearTimeout(timeout1);
         clearTimeout(timeout2);
         clearTimeout(timeout3);
